0fc9cd9544deedfc921db954451876c5ead1fd9c
Mohannad Adham <mohannad.adm@gmail.com>
2024-02-28 04:21:21 +0000

    Truncate tables and build the skeleton of the inserts to table teams

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,19 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+# empty the tables
+echo $($PSQL "TRUNCATE teams, games;")
+
+# iterate over the records of games.csv and insert into the database
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $WINNER != winner ]]
+  then
+    # get team_id of winner
+    # if not found
+      # insert winner as team
+    # get team_id of opponent
+    # if not found
+      # insert opponent as team
+  fi
+done
